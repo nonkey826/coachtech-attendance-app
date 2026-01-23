a33c48ffa58ee414b7deb449f6298b08755a8d7f
nonkey826 <kabosu826n@gmail.com>
2026-01-24 01:10:21 +0900

    Step5: auth header and navigation

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,8 +2,19 @@
     <div class="logo">COACHTECH</div>
 
     <nav class="nav">
-        <a href="{{ route('attendance.index') }}">勤怠</a>
-        <a href="{{ route('attendance.index') }}">勤怠一覧</a>
-        <a href="#">申請</a>
+        @auth
+            <a href="{{ route('attendance.index') }}">勤怠</a>
+            <a href="{{ route('dashboard') }}">ダッシュボード</a>
+
+            <form method="POST" action="{{ route('logout') }}" style="display:inline;">
+                @csrf
+                <button type="submit" class="nav-btn">ログアウト</button>
+            </form>
+        @endauth
+
+        @guest
+            <a href="{{ route('login') }}">ログイン</a>
+            <a href="{{ route('register') }}">会員登録</a>
+        @endguest
     </nav>
 </header>
